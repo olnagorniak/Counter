dd6ff6288d6847bef8dcd7b9955aeabaeb1e38a9
Oleksii Nagorniak <nagorniak.oleksii@gmail.com>
2022-11-29 17:59:20 -0700

    Counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [count, setCount] = React.useState(0);
 
-export default App;
+	const onClickPlus = () => {
+		setCount(count + 1);
+	};
+	const onClickMinus = () => {
+		setCount(count - 1);
+	};
+
+	return (
+	  <div className="App">
+		 <div>
+			<h2>Counter:</h2>
+			<h1>{count}</h1>
+			<button onClick={onClickMinus} className="minus">- Minus</button>
+			<button onClick={onClickPlus} className="plus">Plus +</button>
+		 </div>
+	  </div>
+	);
+ }
+ 
+ export default App;
